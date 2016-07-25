7a33ef2b8a1f7d13af856ef72451b8b34d133556
Narektov <narektov@gmail.com>
2016-07-25 23:20:15 +0400

    MyService Commit first

---
+++ Downloads/Services/app/src/main/java/com/example/narek/services/MyService.java
@@ -0,0 +1,61 @@
+package com.example.narek.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by narek on 7/25/16.
+ */
+public class MyService extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("srv", "Service Stopped");
+
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("srv", "onStartCommand");
+        someTask();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @Override
+    public void onCreate() {
+
+
+        super.onCreate();
+        Log.d("srv", "Service Started");
+    }
+
+    public void someTask() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (Integer i = 0; i < 7; i++) {
+                    Log.d("eee", String.valueOf(i));
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+                stopSelf();
+            }
+        }).start();
+
+    }
+}
